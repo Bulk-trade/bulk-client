616a0cecd3f553fa62ccff82caaf67bb8c3ab559
Jonathan Shore <jonathan.shore@gmail.com>
2026-03-03 13:04:41 -0500

    - new api adjustments

--- python/api/bulk/api/bulk_http.py
+++ python/api/bulk/api/bulk_http.py
@@ -220,7 +220,7 @@ class BulkHttpClient:
             params["aggregation"] = aggregation
 
         response = requests.get(
-            f"{self.base_url}/l2book",
+            f"{self.base_url}/order",
             params=params,
             timeout=self.timeout
         )
@@ -256,7 +256,7 @@ class BulkHttpClient:
             }
         """
         response = requests.post(
-            f"{self.base_url}/account",
+            f"{self.base_url}/order",
             json={
                 "type": "fullAccount",
                 "user": user
